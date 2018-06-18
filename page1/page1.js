cee9ad82d2d178bf16cb01f94bed3992cd778975
atozjames <atozjames@gmail.com>
2018-06-18 23:41:08 +0900

    checkbox

--- page1/page1.js
+++ page1/page1.js
@@ -7,7 +7,26 @@ angular.module("myApp.page1",['ngRoute'])
            controller : "page1Ctrl"     
         });
 
-}]).controller('page1Ctrl',function(){
+}]).controller('page1Ctrl',function($scope){
 
     console.log("page1");
+    $scope.basicCheckValue="true";
+    
+    $scope.getStatus = function(){
+
+        console.log("called getstatus");
+        if($scope.basicCheckValue){
+            console.log("select");
+        }
+
+        if($scope.customValCheckValue){
+            console.log("customValCheckValue select");
+        }
+
+    }
+    
+
+
+
+
 });
